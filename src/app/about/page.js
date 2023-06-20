3c209f5f901b73625a48bb60e9abd1ab7c783bdd
´myhelpsites-com <gabriell_22_@live.com>
2023-06-20 14:38:55 -0300

    create template bootstrap

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+
+export default function About() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <h1>About</h1>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
